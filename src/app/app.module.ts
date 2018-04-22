16934caceee0a32e56f30c830d6e32c8a38a3ccb
Suyash-Purwar <purwarsuyash06@gmail.com>
2018-04-22 07:50:52 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { TodoComponent } from './todo/todo.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TodoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFontAwesomeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+  data: string;
+  todos = [];
+
+  onAdd() {
+    this.todos.push(this.data);
+  }
+
+  onRemoveTodo(id: number) {
+    console.log(id);
+    if (id === 0){
+      this.todos.splice(id, ++id)
+    } else {
+      this.todos.splice(id, id);
+    }
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
